240c34a2b15ca038c17cf01b449f0b639c90b521
LMNxJOHNY <145145650+LMNx9-JOHNY@users.noreply.github.com>
2023-10-24 19:44:42 +0600

    Update gflmn.py

--- gflmn.py
+++ gflmn.py
@@ -1,431 +1,658 @@
 #------------------------------🅳🅰🆁🅺------------------------#
 
+
 #------------------------------🅣🅔🅐🅜------------------------#
+
 
 #-------------------------------𝐋𝐌𝐍𝐱𝟗------------------------------#
 
-import os,random
-
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "Current Time:" + time.asctime(t)
+ 
+print(str)
+import os
+import random
 import time
 
+
+import random.choice
 os.system("pkg install espeak")
+
 os.system('clear')
 
 
+#os.system("xdg-open https://www.facebook.com/profile.php?id=100083728231426&mibextid=ZbWKwL")
+
+# \x1b[38;5;200m  [Deep] beguni
+
+# \x1b[38;5;213m  [Blur] beguni
+
+# \x1b[38;5;202m  [Deep] orange 
+
+# \x1b[38;5;208m  [Blur] orange 
+
+#print(logo)
 #------------------------------🅻🅾🅶🅾------------------------#
 
-# \x1b[38;5;200m  [Deep] beguni
-# \x1b[38;5;213m  [Blur] beguni
-# \x1b[38;5;202m  [Deep] orange 
-# \x1b[38;5;208m  [Blur] orange 
-logo = ("""\x1b[38;5;200m
+logo = ("""\x1b[38;5;202m
 
 (((((((((((((((((((((((((((((((((((((((((((((((((((
 
 (((((((((((((((((((((((((((((((((((((((((((((((((((
 
-------------DARK TEAM LMNx9---------------
+  ----------------DARK TEAM LMNx9-----------------
 
 (((((((((((((((((((((((((((((((((((((((((((((((((((
 
 (((((((((((((((((((((((((((((((((((((((((((((((((((
-
 """)
 
-os.system("xdg-open https://www.facebook.com/profile.php?id=100083728231426&mibextid=ZbWKwL")
-# \x1b[38;5;200m  [Deep] beguni
-# \x1b[38;5;213m  [Blur] beguni
-# \x1b[38;5;202m  [Deep] orange 
-# \x1b[38;5;208m  [Blur] orange 
-#print(logo)
-os.system("espeak -a 300\" What's, Your, Real, Name\"")
-name = input(f'\x1b[38;5;200m╰─⌥⌣❥\x1b[38;5;202m𝐘𝐎𝐔𝐑 𝐍𝐀𝐌𝐄 \x1b[38;5;200m❥⌥⌣⎇ \x1b[38;5;202m')
-os.system("xdg-open https://t.me/+zEnz6yog9AxlNzc9")
+#########-----GIRL------##########
 
+GIRL = random.choice(['https://www.facebook.com/profile.php?id=100082180161925',
 
-#============𝐃𝐀𝐑𝐊 𝐋𝐌𝐍𝐱𝟗============#
-#================🅱🅵================#
-
-def GF():
-
-	wel=f'#\x1b[38;5;200m ❥⌥⌣⎇ \x1b[38;5;202m{name} \x1b[38;5;200m𝐘𝐎𝐔𝐑 \x1b[38;5;202m𝐆𝐈𝐑𝐋𝐅𝐑𝐈𝐄𝐍𝐃\x1b[38;5;200m 𝐈𝐃 𝐋𝐈𝐍𝐊 \x1b[38;5;202m❥⌥⌣⎇ '
-print("⳹🥰💏🥰⳼")
-print(f'\x1b[38;5;202m❥⌥⌣⎇\x1b[38;5;200m{name}\x1b[38;5;202m 𝐘𝐨𝐮𝐫 𝐆𝐢𝐫𝐥𝐅𝐫𝐢𝐞𝐧𝐝 𝐈𝐝 😍 -\n \x1b[38;5;200m❥⌥⌣⎇ \x1b[38;5;202m{GIRL}')
-
-print(" ")
-	
-print("𝐅𝐢𝐧𝐝 𝐌𝐨𝐫𝐞 𝐆𝐈𝐑𝐋𝐅𝐑𝐈𝐄𝐍𝐃 ??? [𝐲/𝐧] ")
-more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
-if more in ["y","Y"]:
-		os.system("python gflmn.py")
-if more in ["n","N"]:
-		print("𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐞 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥")
-		exit()
-
-		#else:
-
-		exit()
-
-			
-GIRL=random.choice(['https://www.facebook.com/profile.php?id=100082180161925',
 
 'https://www.facebook.com/profile.php?id=100075241720582',
 
+
 'https://www.facebook.com/profile.php?id=100078727602108',
+
 
 'https://www.facebook.com/profile.php?id=100069008023528',
 
+
 'https://www.facebook.com/profile.php?id=100061412564815',
+
 
 'https://www.facebook.com/virtuallydeadami',
 
+
 'https://www.facebook.com/profile.php?id=61552071455991',
+
 
 'https://www.facebook.com/profile.php?id=61551243772504',
 
+
 'https://www.facebook.com/profile.php?id=61551230663555',
+
 
 'https://www.facebook.com/shadiya.akter.184881',
 
+
 'https://www.facebook.com/profile.php?id=100094659433254',
 
+
 'https://www.facebook.com/profile.php?id=100090022217467',
+
 
 'https://www.facebook.com/profile.php?id=100095199548021',
 
+
 'https://www.facebook.com/profile.php?id=100094474761592',
+
 
 'https://www.facebook.com/profile.php?id=100094773259597',
 
+
 'https://www.facebook.com/profile.php?id=100094416015847',
+
 
 'https://www.facebook.com/profile.php?id=100094235266029',
 
+
 'https://www.facebook.com/profile.php?id=100091470273236',
+
 
 'https://www.facebook.com/profile.php?id=100094713792349',
 
+
 'https://www.facebook.com/profile.php?id=100090022217467',
+
 
 'https://www.facebook.com/profile.php?id=100070772840003',
 
+
 'https://www.facebook.com/profile.php?id=100076207006669',
+
 
 'https://www.facebook.com/profile.php?id=100094261406496',
 
+
 'https://www.facebook.com/profile.php?id=100094054817703',
+
 
 'https://www.facebook.com/profile.php?id=100085899825498',
 
+
 'https://www.facebook.com/profile.php?id=61550530378641',
+
 
 'https://www.facebook.com/profile.php?id=61550495246136',
 
+
 'https://www.facebook.com/lipikaxr',
+
 
 'https://www.facebook.com/profile.php?id=100094924091516',
 
+
 'https://www.facebook.com/profile.php?id=100094909347953',
+
 
 'https://www.facebook.com/profile.php?id=100095341630877',
 
+
 'https://www.facebook.com/profile.php?id=100090939925080',
+
 
 'https://www.facebook.com/anurupa.2022',
 
+
 'https://www.facebook.com/profile.php?id=100088068836595',
+
 
 'https://www.facebook.com/profile.php?id=100093691474488',
 
+
 'https://www.facebook.com/profile.php?id=100093464484050',
+
 
 'https://www.facebook.com/profile.php?id=100094003810213',
 
+
 'https://www.facebook.com/profile.php?id=100094754044437',
+
 
 'https://www.facebook.com/profile.php?id=100084012092532',
 
+
 'https://www.facebook.com/profile.php?id=100093292137593',
+
 
 'https://www.facebook.com/profile.php?id=100094465284653',
 
+
 'https://www.facebook.com/profile.php?id=61550031182261',
+
 
 'https://www.facebook.com/profile.php?id=100095179839597',
 
+
 'https://www.facebook.com/profile.php?id=100094456762122',
+
 
 'https://www.facebook.com/profile.php?id=100092176804903',
 
+
 'https://www.facebook.com/profile.php?id=100084794002012',
+
 
 'https://www.facebook.com/profile.php?id=100094908997897',
 
+
 'https://www.facebook.com/profile.php?id=100094488024819',
+
 
 'https://www.facebook.com/profile.php?id=100094547589682',
 
+
 'https://www.facebook.com/profile.php?id=100094063866163',
+
 
 'https://www.facebook.com/profile.php?id=100094562860535',
 
+
 'https://www.facebook.com/profile.php?id=61550622321017',
+
 
 'https://www.facebook.com/profile.php?id=61550010308256',
 
+
 'https://www.facebook.com/profile.php?id=61551221250696',
+
 
 'https://www.facebook.com/profile.php?id=100094732804046',
 
+
 'https://www.facebook.com/profile.php?id=100086969974456',
+
 
 'https://www.facebook.com/profile.php?id=100085083327592',
 
+
 'https://www.facebook.com/profile.php?id=100092842971475',
+
 
 'https://www.facebook.com/profile.php?id=100083313745295',
 
+
 'https://www.facebook.com/profile.php?id=100094565023579',
+
 
 'https://www.facebook.com/Nipa.Org',
 
+
 'https://www.facebook.com/profile.php?id=100087938715429',
+
 
 'https://www.facebook.com/profile.php?id=100082336277783',
 
+
 'https://www.facebook.com/profile.php?id=61551583720962',
+
 
 'https://www.facebook.com/profile.php?id=61551546669614',
 
+
 'https://www.facebook.com/profile.php?id=100081664636636',
+
 
 'https://www.facebook.com/profile.php?id=100090270020304',
 
+
 'https://www.facebook.com/profile.php?id=100091355529036',
+
 
 'https://www.facebook.com/profile.php?id=100094815240277',
 
+
 'https://www.facebook.com/profile.php?id=100008527547221',
+
 
 'https://www.facebook.com/profile.php?id=100085287698523',
 
+
 'https://www.facebook.com/profile.php?id=100092281023847',
+
 
 'https://www.facebook.com/profile.php?id=100072179725779',
 
+
 'https://www.facebook.com/alexsoripislam.khan',
+
 
 'https://www.facebook.com/afsana.parvin.5686',
 
+
 'https://www.facebook.com/profile.php?id=100095081166694' 
+
 
 'https://www.facebook.com/profile.php?id=100095327286202',
 
+
 'https://www.facebook.com/profile.php?id=100095473659455',
+
 
 'https://www.facebook.com/profile.php?id=100095385241893',
 
+
 'https://www.facebook.com/profile.php?id=100094681815212',
+
 
 'https://www.facebook.com/profile.php?id=100081558310896',
 
+
 'https://www.facebook.com/profile.php?id=100087231274899',
+
 
 'https://www.facebook.com/israt.jahannur.984786',
 
+
 'https://www.facebook.com/profile.php?id=100089918412099',
+
 
 'https://www.facebook.com/profile.php?id=100092293065740',
 
+
 'https://www.facebook.com/profile.php?id=100092467414234',
+
 
 'https://www.facebook.com/profile.php?id=100094337468246',
 
+
 'https://www.facebook.com/profile.php?id=100095062252684',
+
 
 'https://www.facebook.com/profile.php?id=100093856305381',
 
+
 'https://www.facebook.com/profile.php?id=61550075949269',
+
 
 'https://www.facebook.com/profile.php?id=100091288706976',
 
+
 'https://www.facebook.com/profile.php?id=61550988911551',
+
 
 'https://www.facebook.com/profile.php?id=100093103662620',
 
+
 'https://www.facebook.com/profile.php?id=100081347259576',
+
 
 'https://www.facebook.com/profile.php?id=100059466514426',
 
+
 'https://www.facebook.com/profile.php?id=100071517937274',
+
 
 'https://www.facebook.com/profile.php?id=100093855995047',
 
+
 'https://www.facebook.com/profile.php?id=100089237965710',
+
 
 'https://www.facebook.com/profile.php?id=61550899595526',
 
+
 'https://www.facebook.com/profile.php?id=100092563045635',
+
 
 'https://www.facebook.com/profile.php?id=100079309157825',
 
+
 'https://www.facebook.com/profile.php?id=100078660561921',
+
 
 'https://www.facebook.com/profile.php?id=100093415030055',])
 
+########------BOY------##########
 
-#============𝐃𝐀𝐑𝐊 𝐋𝐌𝐍𝐱𝟗============#
-#================🅱🅵================#
+BOY = random.choice(['https://www.facebook.com/sakep.mohbal.9',
 
-def BF():
-
-	wel=f'#\x1b[38;5;200m ❥⌥⌣⎇ \x1b[38;5;202m{name} \x1b[38;5;200m𝐘𝐎𝐔𝐑 \x1b[38;5;202m𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃\x1b[38;5;200m 𝐈𝐃 𝐋𝐈𝐍𝐊 \x1b[38;5;202m❥⌥⌣⎇ '
-	print(" ⳹🥰💏🥰⳼")
-	print(f'\x1b[38;5;202m❥⌥⌣⎇\x1b[38;5;200m{name}\x1b[38;5;202m 𝐘𝐨𝐮𝐫 𝐁𝐨𝐲𝐅𝐫𝐢𝐞𝐧𝐝 𝐈𝐝 😍 -\n \x1b[38;5;200m❥⌥⌣⎇ \x1b[38;5;202m{BOY}')
-	print(" ")
-	
-	print("𝐅𝐢𝐧𝐝 𝐌𝐨𝐫𝐞 𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃 ??? [𝐲/𝐧] ")
-	more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
-	if more in ["y","Y"]:
-		os.system("python gflmn.py")
-	if more in ["n","N"]:
-		print("𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐞 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥")
-		exit()
-
-		#else:
-
-		exit()
-
-		
-BOY=random.choice(['https://www.facebook.com/sakep.mohbal.9',
 
 'https://www.facebook.com/profile.php?id=100090686956092',
 
+
 'https://www.facebook.com/fahim.vau.3760430',
+
 
 'https://www.facebook.com/profile.php?id=100079576827093',
 
+
 'https://www.facebook.com/profile.php?id=100095163572185',
+
 
 'https://www.facebook.com/profile.php?id=100086782401457',
 
+
 'https://www.facebook.com/profile.php?id=61550008732204',
+
 
 'https://www.facebook.com/profile.php?id=100070978084322',
 
+
 'https://www.facebook.com/profile.php?id=100094869401242',
+
 
 'https://www.facebook.com/profile.php?id=100094626105269',
 
+
 'https://www.facebook.com/profile.php?id=100093435353558',
+
 
 'https://www.facebook.com/original.Typeboss.ur.father',
 
+
 'https://www.facebook.com/profile.php?id=61552258362040',
+
 
 'https://www.facebook.com/profile.php?id=61552074467302',
 
+
 'https://www.facebook.com/salekmahmudofficial',
+
 
 'https://www.facebook.com/desinerraihan',
 
+
 'https://www.facebook.com/copy.link.erorr404',
+
 
 'https://www.facebook.com/D3V1L.N4Y1M',
 
+
 'https://www.facebook.com/DH.Alamin.Hasan01',
+
 
 'https://www.facebook.com/LJ.LMNx9',
 
+
 'https://www.facebook.com/profile.php?id=100082129824201',
+
 
 'https://www.facebook.com/profile.php?id=100082317137320',
 
+
 'https://www.facebook.com/profile.php?id=10008365097585OK',
+
 
 'https://www.facebook.com/REYAL.KING.ARIYAN.𝐘𝐎𝐔𝐑.ABBU.OK',
 
+
 'https://www.facebook.com/profile.php?id=100078790283274' 
+
 
 'https://www.facebook.com/profile.php?id=100078968621002',
 
+
 'https://www.facebook.com/lmnx9.johny',
+
 
 'https://www.facebook.com/mdnhurhossain.hossain',
 
+
 'https://www.facebook.com/mesami.6T9',
+
 
 'https://www.facebook.com/ap.ananda.7906',
 
+
 'https://www.facebook.com/X4JU69',
+
 
 'https://www.facebook.com/ItZ.Ridoy.2k16',
 
+
 'https://www.facebook.com/ridoy.official.buysell',
+
 
 'https://www.facebook.com/xexiridoy2.0',
 
+
 'https://www.facebook.com/ridoyahammedshanto',
+
 
 'https://www.facebook.com/M1NHAJ.M1LL10N',
 
+
 'https://www.facebook.com/nepalimon.013',
+
 
 'https://www.facebook.com/emam.emam.90663',
 
+
 'https://www.facebook.com/yeasien.ahmed.1',
+
 
 'https://www.facebook.com/profile.php?id=100092209298LAGI',
 
+
 'https://www.facebook.com/TODR.ABBU.LAGI',
+
 
 'https://www.facebook.com/profile.php?id=61550768904398',
 
+
 'https://www.facebook.com/profile.php?id=100094140836422',])
 
-os.system('clear')
+#########------NAME-------########
+#============𝐃𝐀𝐑𝐊 𝐋𝐌𝐍𝐱𝟗============#
 
-os.system("xdg-open https://www.facebook.com/profile.php?id=100083728231426&mibextid=ZbWKwL")
+name = input(f'\x1b[38;5;200m╰─⌥⌣❥\x1b[38;5;202m𝐘𝐎𝐔𝐑 𝐍𝐀𝐌𝐄 \x1b[38;5;200m❥⌥⌣⎇ \x1b[38;5;202m')
 
+os.system("espeak -a 300\" What's, Your, Real, Name\"")
+os.system("clear")
 class Main:
 
-		os.system('clear')
-		print (f'\033[1;36m❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥')
-		print(logo)
-		print (f'\033[1;36m❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥')
-		print (f'\033[1;36m ❥[𝟏] [A] 𝐅𝐢𝐧𝐝 𝐘𝐨𝐮𝐫 𝐆𝐈𝐑𝐋𝐅𝐑𝐈𝐄𝐍𝐃')
 
-		print (f'\033[1;36m ❥[𝟐] [B] 𝐅𝐢𝐧𝐝 𝐘𝐨𝐮𝐫 𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃')
-		print (f'\033[1;36m❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥')
-		print (f'\033[1;36m ❥[𝟑] [C] 𝐀𝐃𝐌𝐈𝐍 𝐎𝐧 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊')
-		print (f'\033[1;36m ❥[𝟒] [C] 𝐀𝐃𝐌𝐈𝐍 𝐎𝐧 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝟐')
-		print (f'\033[1;36m ❥[𝟓] [C] 𝐀𝐃𝐌𝐈𝐍 𝐎𝐧 𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌')
-		print (f'\033[1;36m❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥⌥⌣⎇❥')
-		print (f'\033[1;36m ❥[𝟔] [D] 𝐄𝐱𝐢𝐭 𝐓𝐨𝐨𝐥\n')
+#		os.system('clear')
 
-		limon = input(f'\033[1;36m 𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ')
+print (f'\033[1;36m❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥')
 
-		if limon in ["01","1"]:
+print(logo)
 
-			GF()
+print (f'\033[1;36m❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥')
 
-		if limon in ["02","2"]:		
+print (f'\033[1;36m ❥[𝟏]  𝐅𝐢𝐧𝐝 𝐘𝐨𝐮𝐫 𝐆𝐈𝐑𝐋𝐅𝐑𝐈𝐄𝐍𝐃')
 
-			BF()			
+print (f'\033[1;36m ❥[𝟐]  𝐅𝐢𝐧𝐝 𝐘𝐨𝐮𝐫 𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃')
 
-		if limon in ["03","3"]:
+print (f'\033[1;36m❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥')
 
-			os.system("xdg-open https://www.facebook.com/LJ.LMNx9")
-		if limon in ["04","4"]:
-			os.system("xdg-open https://www.facebook.com/lmnx9.johny")
-		if limon in ["05","5"]:
-			os.system("xdg-open https://t.me/+zEnz6yog9AxlNzc9")
-		if limon in ["06","6"]:
-		
-			exit()
+print (f'\033[1;36m ❥[𝟑]  𝐀𝐃𝐌𝐈𝐍 𝐎𝐧 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊')
 
-		else:
+print (f'\033[1;36m ❥[𝟒]  𝐀𝐃𝐌𝐈𝐍 𝐎𝐧 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝟐')
 
-			exit()
+print (f'\033[1;36m ❥[𝟓]  𝐀𝐃𝐌𝐈𝐍 𝐎𝐧 𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌')
+
+print (f'\033[1;36m❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥❥')
+
+print (f'\033[1;36m ❥[𝟔]  𝐄𝐱𝐢𝐭 𝐓𝐨𝐨𝐥\n')
+
+limon = input(f'\033[1;36m 𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ')
+
+if limon in ["01","1"]:
+    GF()
+if limon in ["02","2"]:
+    BF()
+if limon in ["03","3"]:
+    os.system("xdg-open https://www.facebook.com/LJ.LMNx9")
+time.sleep(1)
+print("𝐑𝐮𝐧 𝐀𝐠𝐚𝐢𝐧 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥 ??? [𝐲/𝐧] ")
+            
+
+more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
+
+
+if more in ["y","Y"]:
+	os.system("python gflmn.py")
+       
+if limon in ["04","4"]:
+	os.system("xdg-open https://www.facebook.com/lmnx9.johny")
+
+	time.sleep(1)
+	print("𝐑𝐮𝐧 𝐀𝐠𝐚𝐢𝐧 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥 ??? [𝐲/𝐧] ")
+
+more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
+
+
+if more in ["y","Y"]:
+	os.system("python gflmn.py")
+        
+if limon in ["05","5"]:
+	os.system("xdg-open https://t.me/+zEnz6yog9AxlNzc9")
+
+	time.sleep(1)
+            
+	print("𝐑𝐮𝐧 𝐀𝐠𝐚𝐢𝐧 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥 ??? [𝐲/𝐧] ")
+	more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
+
+
+if more in ["y","Y"]:
+	os.system("python gflmn.py")
+        
+if limon in ["06","6"]:
+    exit()
+    
+else:
+	exit()
+
+
+#============𝐃𝐀𝐑𝐊 𝐋𝐌𝐍𝐱𝟗============#
+
+#================🅶🅵================#
+
+
+def GF():
+
+
+	wel=f'#\x1b[38;5;200m ❥⌥⌣⎇ \x1b[38;5;202m{name} \x1b[38;5;200m𝐘𝐎𝐔𝐑 \x1b[38;5;202m𝐆𝐈𝐑𝐋𝐅𝐑𝐈𝐄𝐍𝐃\x1b[38;5;200m 𝐈𝐃 𝐋𝐈𝐍𝐊 \x1b[38;5;202m❥⌥⌣⎇ '
+
+print("⳹🥰💏🥰⳼")
+print(f'\x1b[38;5;202m❥⌥⌣⎇\x1b[38;5;200m{name}\x1b[38;5;202m 𝐘𝐨𝐮𝐫 𝐆𝐢𝐫𝐥𝐅𝐫𝐢𝐞𝐧𝐝 𝐈𝐝 😍 -\n \x1b[38;5;200m❥⌥⌣⎇ \x1b[38;5;202m{GIRL}')
+
+
+print(" ")
+
+	
+
+print("𝐅𝐢𝐧𝐝 𝐌𝐨𝐫𝐞 𝐆𝐈𝐑𝐋𝐅𝐑𝐈𝐄𝐍𝐃 ??? [𝐲/𝐧] ")
+
+more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
+
+if more in ["y","Y"]:
+
+		os.system("python gflmn.py")
+
+if more in ["n","N"]:
+
+		print("𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐞 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥")
+
+		exit()
+
+
+		#else:
+
+
+		exit()
+
+
+#============𝐃𝐀𝐑𝐊 𝐋𝐌𝐍𝐱𝟗============#
+
+#================🅱🅵================#
+
+
+
+def BF():
+
+
+	wel=f'#\x1b[38;5;200m ❥⌥⌣⎇ \x1b[38;5;202m{name} \x1b[38;5;200m𝐘𝐎𝐔𝐑 \x1b[38;5;202m𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃\x1b[38;5;200m 𝐈𝐃 𝐋𝐈𝐍𝐊 \x1b[38;5;202m❥⌥⌣⎇ '
+
+	print(" ⳹🥰💏🥰⳼")
+
+	print(f'\x1b[38;5;202m❥⌥⌣⎇\x1b[38;5;200m{name}\x1b[38;5;202m 𝐘𝐨𝐮𝐫 𝐁𝐨𝐲𝐅𝐫𝐢𝐞𝐧𝐝 𝐈𝐝 😍 -\n \x1b[38;5;200m❥⌥⌣⎇ \x1b[38;5;202m{BOY}')
+
+	print(" ")
+
+	
+
+	print("𝐅𝐢𝐧𝐝 𝐌𝐨𝐫𝐞 𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃 ??? [𝐲/𝐧] ")
+
+	more = input("𝐂𝐇𝐎𝐎𝐒𝐄⌥⌣⎇❥ ")
+
+	if more in ["y","Y"]:
+
+		os.system("python gflmn.py")
+
+	if more in ["n","N"]:
+
+		print("𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐞 𝐋𝐌𝐍𝐱𝟗 𝐓𝐨𝐨𝐥")
+
+		exit()
+
+
+		#else:
+
+
+		exit()
+        
+
+#os.system('clear')
+
+#os.system("xdg-open https://www.facebook.com/profile.php?id=100083728231426&mibextid=ZbWKwL")
+
 
 Main()
