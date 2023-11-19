02efc8adb59e2d02f0a4c1b3cabccea46d3a45f2
DARK_TEAM_LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2023-11-19 20:33:36 +0600

    Update UA project.py

--- UA project.py
+++ UA project.py
@@ -8,8 +8,8 @@ import os
 
 def DARK_LMNx9():
     user_agents = []
-    user_agent = f"[FBAN/FB4A;FBAV/"+str(random.randint(199,399))+".0.0."+str(random.randint(1,9))+"."+str(random.randint(99,199))+";FBBV/"+str(random.randint(111111111,999999999))+";FBDM/{density="+str(random.randint(2,3))+"."+str(random.randint(2,5))+",width=1080,height="+str(random.randint(1400,1499))+"};FBLC/"+str(random.choice(["cs_CZ","en_GB","en_US","lt_LT","pl_PL","id_ID","ru_RU","pt_PT","he_IL","hi_IN","nl_NL"," it_IT","en_IN","es_ES","en_PK"]))+";FBRV/"+str(random.randint(111111111,999999999))+";FBCR/"+str(random.choice(["Tele2You","Telenor","FASTWEB","Banglalink","Sprint","Jazz","Vodafone IN","Vi India","Tele2 LT","Jio 4G","EE","Oi","MtelBG","AT&amp;amp-T","Ufone","Azercell"]))+";FBMF/Xiaomi;FBBD/"+str(random.choice(["xiaomi","Xiaomi","Redmi"]))+";FBPN/com.facebook.katana;FBDV/"+str(random.choice(["M2101K7AG","M2003J15SC","M2004J19C","M2006C3LG","M2007J20CG","M2010J19CG","M2011K2C","M2012K11AG","M2101K7BG","M2101K9G","M2102J20SG","M2102K1G","M2003J15SC","MI MAX 2","AT&amp;amp-T","Redmi Note 4", "Redmi 4X","Redmi Note 8 Pro","Redmi Note 5","Redmi Note 8T","Redmi 6A","Redmi 7A","MI PLAY","MI 8 Lite","Mi 9T","Mi A2 Lite"," Mi 9", "M2101K7AG"]))+";FBSV/"+str(random.randint(9,12))+";FBOP/1;FBCA/arm64-v8a:;]"
-    user_agents.append(user_agent)
+    user_agent = fl[FBAN/FB4A;FBAV/"+str(random.randint(199,399))+".0.0."+str(random.randint(1,9))+"."+str(random.randint(99,199))+";FBBV/"+str(random.randint(111111111,999999999))+";FBDM/{density="+str(random.randint(2,3))+"."+str(random.randint(2,5))+",width=1080,height="+str(random.randint(1400,1499))+"};FBLC/"+str(random.choice(["cs_CZ","en_GB","en_US","lt_LT","pl_PL","id_ID","ru_RU","pt_PT","he_IL","hi_IN","nl_NL"," it_IT","en_IN","es_ES","en_PK"]))+";FBRV/"+str(random.randint(111111111,999999999))+";FBCR/"+str(random.choice(["Tele2You","Telenor","FASTWEB","Banglalink","Sprint","Jazz","Vodafone IN","Vi India","Tele2 LT","Jio 4G","EE","Oi","MtelBG","AT&amp;amp-T","Ufone","Azercell"]))+";FBMF/Xiaomi;FBBD/"+str(random.choice(["xiaomi","Xiaomi","Redmi"]))+";FBPN/com.facebook.katana;FBDV/"+str(random.choice(["M2101K7AG","M2003J15SC","M2004J19C","M2006C3LG","M2007J20CG","M2010J19CG","M2011K2C","M2012K11AG","M2101K7BG","M2101K9G","M2102J20SG","M2102K1G","M2003J15SC","MI MAX 2","AT&amp;amp-T","Redmi Note 4", "Redmi 4X","Redmi Note 8 Pro","Redmi Note 5","Redmi Note 8T","Redmi 6A","Redmi 7A","MI PLAY","MI 8 Lite","Mi 9T","Mi A2 Lite"," Mi 9", "M2101K7AG"]))+";FBSV/"+str(random.randint(9,12))+";FBOP/1;FBCA/arm64-v8a:;]"
+
     return user_agents
 
 android_user_agents = DARK_LMNx9()
@@ -23,7 +23,7 @@ for user_agent in android_user_agents:
 
 print('\n\nExample : 5_ 20_ 10')
 
-limit = input('Enter Limit :')
+limit = inpt('Enter Limit :')
 
 #_______UA--2_________
 if limit in ['2']:
@@ -34,19 +34,19 @@ if limit in ['2']:
 #_______UA--5_________
 if limit in ['5','05']:
     for _ in range(5):
-        colour = random.choice(['\033[91m','\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
+        colour = random.choice(['\033[1m','\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
         print('\n'+'\033[1;31m┃😈┃\033[47m\033[1;46m   𝗧𝗛𝗘 𝗖𝗬𝗕𝗘𝗥 𝗠𝗔𝗙𝗜𝗔    ➤   𝗗𝗔𝗥𝗞 𝗧𝗘𝗔𝗠 𝗟𝗠𝗡𝘅𝟗   \033[40m\033[00m\x1b[1;91m┃😈┃\n'+'\n'+colour+user_agent)
 
 #_______UA--10_________
 if limit in ['10']:
     for _ in range(10):
-        colour = random.choice(['\033[91m','\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
+        colour = random.choice(['\033[91m'\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
         print('\n'+'\033[1;31m┃😈┃\033[47m\033[1;46m   𝗧𝗛𝗘 𝗖𝗬𝗕𝗘𝗥 𝗠𝗔𝗙𝗜𝗔    ➤   𝗗𝗔𝗥𝗞 𝗧𝗘𝗔𝗠 𝗟𝗠𝗡𝘅𝟗   \033[40m\033[00m\x1b[1;91m┃😈┃\n'+'\n'+colour+user_agent)
         
 #_______UA--20_________
 if limit in ['20']:
-    for _ in range(20):
-        colour = random.choice(['\033[91m','\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
+    for _ in rage(20)
+        colour = random.coice(['\033[91m','\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
         print('\n'+'\033[1;31m┃😈┃\033[47m\033[1;46m   𝗧𝗛𝗘 𝗖𝗬𝗕𝗘𝗥 𝗠𝗔𝗙𝗜𝗔    ➤   𝗗𝗔𝗥𝗞 𝗧𝗘𝗔𝗠 𝗟𝗠𝗡𝘅𝟗   \033[40m\033[00m\x1b[1;91m┃😈┃\n'+'\n'+colour+user_agent)
 
 #_______UA--30_________
@@ -65,7 +65,7 @@ if limit in ['40']:
 #_______UA--50_________
 if limit in ['50']:
     for _ in range(50):
-        colour = random.choice(['\033[91m','\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
+        colour = random.chice(['\033[91m'\033[92m','\033[93m','\033[94m','\033[95m','\033[96m'])
         print('\n'+'\033[1;31m┃😈┃\033[47m\033[1;46m   𝗧𝗛𝗘 𝗖𝗬𝗕𝗘𝗥 𝗠𝗔𝗙𝗜𝗔    ➤   𝗗𝗔𝗥𝗞 𝗧𝗘𝗔𝗠 𝗟𝗠𝗡𝘅𝟗   \033[40m\033[00m\x1b[1;91m┃😈┃\n'+'\n'+colour+user_agent)
  
 #_______UA--60_________
